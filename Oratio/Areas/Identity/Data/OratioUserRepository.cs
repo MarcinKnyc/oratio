029baededca79f46eb9be52f9243002e225ac8f6
Marcin Knyc <maroxesen@gmail.com>
2023-01-07 16:12:45 +0100

    feat: currentUserRepository

---
+++ Oratio/Areas/Identity/Data/OratioUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using Oratio.Data;
+
+namespace Oratio.Areas.Identity.Data
+{
+    public class OratioUserRepository
+    {
+        private readonly UserContext _userContext;
+        public OratioUserRepository(UserContext userContext) 
+        {
+            _userContext = userContext;
+        }
+
+        public OratioUser? FetchUserById(Guid id)
+        {
+            return _userContext.Users.FirstOrDefault(user => user.Id == id.ToString());
+        }
+    }
+}
